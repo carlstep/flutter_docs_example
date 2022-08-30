22d821429614ecdf96f87b720d97e3be3e0a5d1a
carlstep <carlstep333@me.com>
2022-08-30 11:35:59 +0700

    setup material_components page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_docs_example/pages/material_components.dart';
 import 'pages/basic_widgets.dart';
 
 void main() {
   runApp(MaterialApp(
-    initialRoute: '/',
+    initialRoute: '/b',
     routes: {
       '/a': (context) => const BasicWidgets(),
+      '/b': (context) => const TutorialHome(),
     },
     home: const SafeArea(
       child: BasicWidgets(),
--- lib/pages/material_components.dart
+++ lib/pages/material_components.dart
@@ -7,10 +7,28 @@ class TutorialHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
+        leading: const IconButton(
           onPressed: null,
           icon: Icon(Icons.menu),
         ),
+        title: const Text(
+          'Example Title',
+        ),
+        actions: const [
+          IconButton(
+            onPressed: null,
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text('Hello World'),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        elevation: 0,
+        onPressed: null,
+        child: Icon(Icons.add),
       ),
     );
   }
